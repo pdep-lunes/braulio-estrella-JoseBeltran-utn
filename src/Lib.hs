df32f840f3892621761e5a00af919304c6446160
JoseBeltran-UTN <jbeltranflores@frba.utn.edu.ar>
2025-04-28 12:10:26 -0300

    se agrego funciones bolaEspinosa y lluviaDeTuercas

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,12 +7,22 @@ data Personaje = unPersonaje {
     poderBasico :: String,
     superPoder :: String,
     superPoderActivo :: Bool,
-    cantidadVida :: Int
+    cantidadVida :: Int,
+    estado :: String
 }
 
 doble :: Int -> Int
 doble x = x * 2
 
 bolaEspinosa :: Personaje -> Int
-bolaEspinosa unPersonaje
-    | 
+bolaEspinosa unPersonaje 
+    | resultadoVidaNegativa unPersonaje = 0
+    | otherwise = cantidadVida unPersonaje - 1000
+
+-- resultadoVidaNegativa: cantidadVida unPersonaje <= 1000
+
+lluviaDeTuercas :: Personaje -> Int
+lluviaDeTuercas unPersonaje
+    | esColega unPersonaje = cantidadVida unPersonaje + 800
+    | esContrincante unPersonaje = cantidadVida unPersonaje `div` 2
+    | otherwise = cantidadVida unPersonaje
