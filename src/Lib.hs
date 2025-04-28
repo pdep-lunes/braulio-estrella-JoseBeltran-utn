c887ce880262ec577395c6ca6136cf8135a39c87
JoseBeltran-UTN <jbeltranflores@frba.utn.edu.ar>
2025-04-28 11:20:18 -0300

    1er commit de prueba

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,17 @@ module Lib () where
 
 import Text.Show.Functions ()
 
+data Personaje = unPersonaje {
+    nombre :: String,
+    poderBasico :: String,
+    superPoder :: String,
+    superPoderActivo :: Bool,
+    cantidadVida :: Int
+}
+
 doble :: Int -> Int
 doble x = x * 2
+
+bolaEspinosa :: Personaje -> Int
+bolaEspinosa unPersonaje
+    | 
